ef0cccb41c5c35326b29b470fe904395690b860c
recheeduardo <eduardorechemartins2006@gmail.com>
2026-06-16 14:51:00 -0300

    fix: application streamline exception

--- gruapim-backend/src/main/java/com/gruapim/GruapimApplication.java
+++ gruapim-backend/src/main/java/com/gruapim/GruapimApplication.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 public class GruapimApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GruapimApplication.class, args);
     }
